b29cbab4b1492916046fa01d07c34ed7db9e825f
Ana Lemos <carol1692@hotmail.com>
2025-10-25 16:17:14 -0300

    remoção dotenv

--- src/main/java/com/generation/blogpessoal/BlogpessoalApplication.java
+++ src/main/java/com/generation/blogpessoal/BlogpessoalApplication.java
@@ -3,29 +3,15 @@ package com.generation.blogpessoal;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import io.github.cdimascio.dotenv.Dotenv;
+//import io.github.cdimascio.dotenv.Dotenv;
 
 @SpringBootApplication
 public class BlogpessoalApplication {
 
 	public static void main(String[] args) {
-		 try {
-	            // Tenta carregar o .env (para uso local)
-	            Dotenv dotenv = Dotenv.configure()
-	                                  .ignoreIfMissing()  // Evita erro se o arquivo não existir
-	                                  .load();
-
-	            // Carrega variáveis locais no System se existirem
-	            dotenv.entries().forEach(entry -> {
-	                if (System.getenv(entry.getKey()) == null) {
-	                    System.setProperty(entry.getKey(), entry.getValue());
-	                }
-	            });
-
-	            System.out.println("✅ Variáveis de ambiente carregadas com sucesso!");
-	        } catch (Exception e) {
-	            System.out.println("⚠️ Nenhum arquivo .env encontrado (modo produção)");
-	        }
+//		Dotenv dotenv = Dotenv.load();
+//
+//		dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
 		
 		SpringApplication.run(BlogpessoalApplication.class, args);
 	}
